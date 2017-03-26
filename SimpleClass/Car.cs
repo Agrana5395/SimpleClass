191f7e05315a1a43be9268cb3fc14d2188db80c6
Alfredo Granado <alfred_grand@outlook.com>
2017-03-26 19:56:09 -0400

    easy change clase

---
+++ SimpleClass/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClass
+{
+    class Car
+    {
+            public string Make { get; set; }
+            public string Model { get; set; }
+            public int Year { get; set; }
+            public string Color { get; set; }
+
+        public decimal DetermineMarketValue()
+        {
+            decimal carValue;
+            if (Year > 1990)
+            {
+                carValue = 11000;
+            }
+            else
+            {
+                carValue = 200;
+            }
+            return carValue;
+        }
+       
+        // The constructor.
+        public Car()
+        {
+            //youn could load from a config file or a database
+            this.Make = "Nissan";
+        }
+
+        public Car(string make, string model, int year, string color)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.Color = color;
+
+        }
+    }
+}
--- SimpleClass/Program.cs
+++ SimpleClass/Program.cs
@@ -10,6 +10,58 @@ namespace SimpleClass
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Please tell me the year of your car");
+            int year = Convert.ToInt16(Console.ReadLine());
+
+            Car myCar = new Car();
+            decimal value = DetermineMarketValue(myCar);
+            myCar.Make = "Oldsmobile";
+            myCar.Make = "Cutlas Supreme";
+            myCar.Year = year;
+            myCar.Color = "Sylver";
+
+            Console.WriteLine(" {0} {1} {2} {3} ",
+               myCar.Make,
+               myCar.Model,
+               myCar.Year,
+               myCar.Color);
+      
+            Console.WriteLine("{0:C}", myCar.DetermineMarketValue());
+
+
+            Car myCar3 = new Car("Tesla", "S model", year, "Red");
+            decimal valueCar3 = DetermineMarketValue(myCar3);
+            Console.WriteLine("The new car is {0} from  {1}, and cost just {2:C}",
+                myCar3.Model, myCar3.Year, myCar3.DetermineMarketValue());
+
+
+            
+
+            //Car myCar2 = new Car();
+            //myCar2 = myCar;
+            //myCar2.Model = "V4c";
+            //Console.WriteLine("The second car change the model {0}", myCar2.Model);
+            //Console.WriteLine("--------------------------");
+
+
+            // I Did comment out the objects in orther to test the new constructor in class Car
+            //please, make sure you remove the constructor in  the Car Class if you uncoment (above code)
+
+           
+               Console.ReadLine();
+
+
+            
+
+        }
+
+        private static decimal DetermineMarketValue(Car car)
+        {
+            decimal carValue = 100.0M;
+
+            return carValue;
         }
     }
+
 }
